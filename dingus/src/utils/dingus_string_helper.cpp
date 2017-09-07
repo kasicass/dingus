4b16312bb3a4675ead7ebd3401ade42d1f9a46d9
kasicass <kasicass@gmail.com>
2017-09-07 11:43:30 +0800

    StringHelper

--- dingus/src/utils/dingus_string_helper.cpp
+++ dingus/src/utils/dingus_string_helper.cpp
@@ -11,3 +11,92 @@ void CStringHelper::replaceTabs(std::string &s)
 	}
 }
 
+void CStringHelper::replaceLineEnds(std::string &s)
+{
+	size_t n = s.size();
+	for (size_t i = 0; i < n; ++i)
+	{
+		if (s[i] == '\r' || s[i] == '\n') s[i] = ' ';
+	}
+}
+
+void CStringHelper::replaceWhitespace(std::string &s)
+{
+	size_t n = s.size();
+	for (size_t i = 0; i < n; ++i)
+	{
+		if (!isprint(s[i])) s[i] = ' ';
+	}
+}
+
+void CStringHelper::trimString(std::string &s)
+{
+	size_t first = s.find_first_not_of(" \t\n\r");
+	if (first == std::string::npos)
+	{
+		s = "";
+		return;
+	}
+
+	size_t last = s.find_last_not_of(" \t\n\r");
+	s = s.substr(first, (last-first+1));
+}
+
+int CStringHelper::countLines(const std::string &s)
+{
+	int c = 1;
+	size_t n = s.size();
+	for (size_t i = 0; i < n; ++i)
+	{
+		if (s[i] == '\n') ++c;
+	}
+	return c;
+}
+
+bool CStringHelper::isLetter(char c)
+{
+	return !!isalpha(c);
+}
+
+bool CStringHelper::isDigit(char c)
+{
+	return !!isdigit(c);
+}
+
+bool CStringHelper::startsWith(const std::string &s, const std::string &subs)
+{
+	return s.find(subs) == 0;
+}
+
+bool CStringHelper::endsWith(const std::string &s, const std::string &subs)
+{
+	return s.find(subs) == (s.length() - subs.length());
+}
+
+char CStringHelper::toLower(char c)
+{
+	return (char)tolower(c);
+}
+
+char CStringHelper::toUpper(char c)
+{
+	return (char)toupper(c);
+}
+
+void CStringHelper::toLower(std::string &s)
+{
+	size_t n = s.size();
+	for (size_t i = 0; i < n; ++i)
+	{
+		s[i] = (char)tolower(s[i]);
+	}
+}
+
+void CStringHelper::toUpper(std::string &s)
+{
+	size_t n = s.size();
+	for (size_t i = 0; i < n; ++i)
+	{
+		s[i] = (char)toupper(s[i]);
+	}
+}
--- dingus/src/utils/dingus_string_helper.hpp
+++ dingus/src/utils/dingus_string_helper.hpp
@@ -7,7 +7,24 @@ namespace dingus {
 class CStringHelper
 {
 public:
-	static void replaceTabs(std::string &s);      // replaces tabs with single spaces
+	static void replaceTabs(std::string &s);         // replaces tabs with single spaces
+	static void replaceLineEnds(std::string &s);     // replaces line ends with single spaces
+	static void replaceWhitespace(std::string &s);   // replaces white space with single spaces
+
+	static void trimString(std::string &s);          // trims whitespace from ends
+	static int countLines(const std::string &s);     // counts number of lines
+
+	static bool isLetter(char c);
+	static bool isDigit(char c);
+
+	static bool startsWith(const std::string &s, const std::string &subs);
+	static bool endsWith(const std::string &s, const std::string &subs);
+
+	static char toLower(char c);
+	static char toUpper(char c);
+	static void toLower(std::string &s);
+	static void toUpper(std::string &s);
+
 };
 
 }
